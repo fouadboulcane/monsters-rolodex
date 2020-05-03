8570e045362157d00907d24a6ee2591266928753
fouadlee <fouadlee9@gmail.com>
2020-05-03 11:44:09 +0200

    Updates

---
+++ precache-manifest.28b7171284e769a4606c7a415c430f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9717a2d9c04c5d9097ff784fadc7ec2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3e14ef56b3932821dcb0",
+    "url": "/monsters-rolodex/static/css/main.d830cf74.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e14ef56b3932821dcb0",
+    "url": "/monsters-rolodex/static/js/main.e4d608d0.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
